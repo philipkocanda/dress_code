20f10cc9983f63a475f20526f34188707f0b9d9f
Philip Kocanda <philip@hackerone.com>
2015-06-05 17:19:30 +0200

    React/CJSX Support

--- lib/dress_code/renderer.rb
+++ lib/dress_code/renderer.rb
@@ -4,18 +4,43 @@ require_relative '../dress_code'
 
 class DressCode::Renderer < Redcarpet::Render::HTML
 
+  attr_reader :cjsx_code_blocks
+
+  def initialize
+    super
+    @cjsx_code_blocks = []
+  end
+
+  def table(header, body)
+    "<table class=\"table\">" \
+      "#{header}#{body}" \
+    "</table>"
+  end
+
   def block_code(code, language)
+    render_language = language
+    render_language = 'html' if language == 'cjsx'
+
     syntax = Pygments.highlight(code, {
-      lexer: language,
+      lexer: render_language,
       options: { encoding: 'utf-8' }
     })
+
     inner = if language == 'html'
       "#{syntax} <div class='code-rendered'>#{code}</div>"
+    elsif language == 'cjsx'
+      index = @cjsx_code_blocks.length
+      @cjsx_code_blocks << code
+
+      "#{syntax}
+      <div class='code-rendered'>
+        <div class='cjsx-rendered-#{index}'>cjsx-rendered-#{index}</div>
+      </div>"
     else
       syntax
     end
-    "#{inner}"
-  end
 
+    "<div class='code-demo'>#{inner}</div>"
+  end
 end
 
